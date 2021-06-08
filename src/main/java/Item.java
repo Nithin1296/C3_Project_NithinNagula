606373deffbe3c8c3d84874192c66d7576cec210
Nithin1296 <nithin.nik44@gmail.com>
2021-06-09 00:18:02 +0530

    "Part 2 Solution"

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -5,8 +5,7 @@ import java.util.List;
 public class Item {
     private String name;
     private int price;
-    String selection;
-
+    
     public Item(String name, int price) {
         this.name = name;
         this.price = price;
@@ -25,26 +24,6 @@ public class Item {
     public int getPrice(){
         return price;
     }
-//    public void setSelectedItem(String name) {
-//       // name = Arrays.asList(name);
-//        selection = (String) name;
-//    }
-//    public String getSelectedItem(){
-//        return selection;
-//    }
-//    public int calculateCost(List Items){
-//
-//       Restaurant restaurant = new Restaurant("Amelie cae","Hyd", LocalTime.of(10,00),LocalTime.of(22,00));
-//       int count = restaurant.getMenu().size();
-//        for(int i=0; i<count;i++){
-//            if(getName()==selection){
-//                price+=price;
-//
-//            }
-//        }
-//
-//            return price;
-//    }
 
     }
 
